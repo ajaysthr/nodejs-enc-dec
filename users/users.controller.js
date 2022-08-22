d3a48a2d3880a95f85bc8ef7410d07d6375a2f93
ajay suthar <ajaysthr@gmail.com>
2022-08-23 00:27:24 +0530

    ini

---
+++ users/users.controller.js
@@ -0,0 +1,105 @@
+﻿const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const Role = require('_helpers/role');
+const userService = require('./user.service');
+const CryptoJS = require("crypto-js");
+// routes
+
+router.get('/', getAll);
+router.get('/:id', getById);
+// router.post('/', createSchema, create);
+router.post('/', create);
+router.put('/:id', updateSchema, update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+// route functions
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function create(req, res, next) {
+    console.log('req body ***---', JSON.stringify(req.body));
+    let name = req.body.name;
+    let email = req.body.email;
+    name = decrypt(name);
+    email = decrypt(email);
+    console.log('name in create ***---', name);
+    console.log('email in create ***---', email);
+    //userService.create(req.body)
+    //    .then(() => res.json({ message: 'User created' }))
+    //    .catch(next);
+}
+
+function decrypt(text){
+    var key = '6fa979f20126cb08aa645a8f495f6d85';
+    var iv  = '7777777a72ddc2f1';
+
+    var decrypted = CryptoJS.AES.decrypt(text, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(iv),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    console.log('decrypted ----****---',decrypted.toString(CryptoJS.enc.Utf8));
+    return decrypted.toString(CryptoJS.enc.Utf8);
+    
+    // let key = '6fa979f20126cb08aa645a8f495f6d85';
+    // let iv = '7777777a72ddc2f1';
+    // let cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+    //     iv: CryptoJS.enc.Utf8.parse(iv),
+    //     padding: CryptoJS.pad.Pkcs7,
+    //     mode: CryptoJS.mode.CBC
+    // });
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(() => res.json({ message: 'User updated' }))
+        .catch(next);
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({ message: 'User deleted' }))
+        .catch(next);
+}
+
+// schema functions
+
+function createSchema(req, res, next) {
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        role: Joi.string().valid(Role.Admin, Role.User).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+        title: Joi.string().empty(''),
+        firstName: Joi.string().empty(''),
+        lastName: Joi.string().empty(''),
+        role: Joi.string().valid(Role.Admin, Role.User).empty(''),
+        email: Joi.string().email().empty(''),
+        password: Joi.string().min(6).empty(''),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
+    }).with('password', 'confirmPassword');
+    validateRequest(req, next, schema);
+}
